f42aee12cb2a402c95594368c5bd8a37bd819b81
unknown <vassaeve@gmail.com>
2018-02-06 23:34:18 +0300

    first commit

---
+++ cos/ejb1/src/com/mycompany/ejb1/NewSessionBean.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejb1;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author admin
+ */
+@Stateless
+public class NewSessionBean implements NewSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ cos/ejb1/src/com/mycompany/ejb1/NewSessionBeanLocal.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejb1;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author admin
+ */
+@Local
+public interface NewSessionBeanLocal {
+    
+}
